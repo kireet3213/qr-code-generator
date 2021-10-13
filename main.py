d02c926dcfb79fc9b7fe8f5f59c95c8afc0e781b
kireet3213 <92445686+kireet3213@users.noreply.github.com>
2021-10-14 00:42:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -37,8 +37,30 @@ def make_logo_qr(str,logo,save):
 
     # add logo
     if logo and os.path.exists(logo):
-      pass
+        icon=Image.open(logo)
+        # get size of QRCode image
+        img_w,img_h=img.size
 
+        factor=4
+        size_w=int(img_w/factor)
+        size_h=int(img_h/factor)
+
+        # size of logo <= 1/4 * QRCode image
+        icon_w,icon_h=icon.size
+        if icon_w>size_w:
+            icon_w=size_w
+        if icon_h>size_h:
+            icon_h=size_h
+        icon=icon.resize((icon_w,icon_h),Image.ANTIALIAS)
+        # see http://pillow.readthedocs.org/handbook/tutorial.html
+
+        # compute the position of logo in output image
+        w=int((img_w-icon_w)/2)
+        h=int((img_h-icon_h)/2)
+        icon=icon.convert("RGBA")
+        # paste logo on the output image
+        img.paste(icon,(w,h),icon)
+        # see：http://pillow.readthedocs.org/reference/Image.html#PIL.Image.Image.paste
     # save QRCode image
     img.save(save)
 
